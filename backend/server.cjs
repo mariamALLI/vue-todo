f83b27d55aacb658799fc35cee8406eebe0050cb
Mariam Alli <mariam_oa@yahoo.com>
2025-09-22 23:47:00 +0100

    commented out some code

--- backend/server.cjs
+++ backend/server.cjs
@@ -7,10 +7,11 @@ const mongoose = require('mongoose');
 // Use environment variable for MongoDB (never hardcode in cloud)
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/vue-todo';
 mongoose.connect(MONGO_URI);
+mongoose.connect('mongodb+srv://mariamalli_db_user:foLXx8TF82djgXOk@todo-backend.hucsc9o.mongodb.net/vue-todo?retryWrites=true&w=majority&appName=todo-backend')
 
 // MongoDB Models
-const TodoSchema = new mongoose.Schema({ text: String, completed: Boolean });
-const Todo = mongoose.model('Todo', TodoSchema);
+// const TodoSchema = new mongoose.Schema({ text: String, completed: Boolean });
+// const Todo = mongoose.model('Todo', TodoSchema);
 
 const ChatSchema = new mongoose.Schema({ text: String, time: String, username: String, avatar: String });
 const ChatMsg = mongoose.model('ChatMsg', ChatSchema);
@@ -43,27 +44,27 @@ const io = socketIo(server, {
 });
 
 io.on('connection', async (socket) => {
-  socket.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
+  // socket.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
   socket.emit('chatUpdate', await ChatMsg.find().sort({ _id: 1 }));
 
-  socket.on('addTodo', async (todo) => {
-    await new Todo(todo).save();
-    io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
-  });
+  // socket.on('addTodo', async (todo) => {
+  //   await new Todo(todo).save();
+  //   io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
+  // });
 
-  socket.on('deleteTodo', async (id) => {
-    await Todo.deleteOne({ _id: id });
-    io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
-  });
+  // socket.on('deleteTodo', async (id) => {
+  //   await Todo.deleteOne({ _id: id });
+  //   io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
+  // });
 
-  socket.on('toggleTodo', async (id) => {
-    const todo = await Todo.findById(id);
-    if (todo) {
-      todo.completed = !todo.completed;
-      await todo.save();
-      io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
-    }
-  });
+  // socket.on('toggleTodo', async (id) => {
+  //   const todo = await Todo.findById(id);
+  //   if (todo) {
+  //     todo.completed = !todo.completed;
+  //     await todo.save();
+  //     io.emit('todosUpdate', await Todo.find().sort({ _id: -1 }));
+  //   }
+  // });
 
   socket.on('sendMessage', async (msg) => {
     await new ChatMsg(msg).save();
@@ -72,9 +73,9 @@ io.on('connection', async (socket) => {
 });
 
 // Optional REST endpoints
-app.get('/api/todos', async (req, res) => {
-  res.json(await Todo.find().sort({ _id: -1 }));
-});
+// app.get('/api/todos', async (req, res) => {
+//   res.json(await Todo.find().sort({ _id: -1 }));
+// });
 
 app.get('/api/chats', async (req, res) => {
   res.json(await ChatMsg.find().sort({ _id: 1 }));
